6eb94f3d5f227374412fa3b83d4d9aa092e3094a
Irvin Choi <simdol8080@gmail.com>
2018-12-23 09:17:54 -0700

    initial population

---
+++ ReadMe.md
@@ -0,0 +1,69 @@
+# SolderPackager - Makes packaging mods for Technic Solder easy!
+
+# # Description
+
+Unlike other modpack distribution system, TechnicLauncher allows modularizing mods for a modpack. This is awesome, because it allows users to only have to download changes when modpack is updated, without having to download the whole pack again. However, due to the nature of the TechnicSolder and its repository database schemea, modpack developers must package mods individually, which can be very time consuming. 
+
+While there are other existing tools to help you package your mods, most of them are either abandoned, not cross-platform, not automated enough, or simply doesn't work well. This program is written in Ruby, which is very flexible and can run on many different platforms since Ruby doesn't have a compiler. Although it is not tested, this should run fine on macOS or Windows as long as environment for Ruby is setup properly.
+
+This program is under development. Currently it lacks uploading feature and updating database for Solder automatically upon upload. It can however take all mods in a given folder, generate a nice configuration file from mcmod.info/litemod.json and with minimal modification, which this program will use the config to create Solder-ready .zip file, so that all you have to do is upload them and update the Solder database afterwards.
+
+# # License
+
+This program is licensed with GPL v2.0. This means that you can use it however you want, but if you decide to change the code, you are required to put a link to the original source (this page) and share the code you've changed from the original. This is so that people can easily use the program without having to worry about license and to allow this program to be developed without being abandoned.
+
+# # Running
+
+There are few requirements for running this program. Although the environment can vary and the program might run on a different environnment than it is listed on here, this is what is officially supported.
+
+&nbsp; | Supported Versions
+----: | :----------
+Ruby | 2.5 and above
+Ruby | Must have 'rubyzip' gem installed. 
+OS | GNU/Linux, Windows 7 and above, macOS Yosemite and above
+
+To run this program, simply clone this repository, add mods to 'mods' folder and run "ruby run.rb". This should automatically grab mods from the mods folder and generate appropriate configuration file. It will prompt you to check configuration file at config/mods.yml, which you should than check for any mods that has "N/A" in "version:". Next, you should get the version of the mod, change version and trim the modname so that it only includes modname.
+
+For instance:
+
+iamamod-1.2.jar:
+   version: N/A
+   filename: iamamod-1.2.jar
+   
+Should be:
+    
+    iamamod:
+    version: 1.2
+    filename: iamamod-1.2.jar.
+    
+As this program grabs information from mcmod.info/litemod.json, you shouldn't have to do this for most of your mods. This is just a workaround for mods that doesn't have necessary information, or when that file is not accessible.
+
+# # Bugs & Issues
+
+Any bugs and issues should be reported at "Issues" tab, with following information:
+
+- Your operating system: 
+- Ruby version:
+- Issue:
+
+Without the information above, I will not be able to reproduce the issue, which will make it impossible for me to resolve. You are strongly encouraged to give as detailed information regarding bug/issue as in depth as possible. 
+
+# # Feature Requests
+
+You are more than welcome to open a new requst for features in the "Issues" tab. If you could include how to implement that feature and why, it would make my life much easier. Even better, submit a pull request!
+
+# # Pulls (Pull Request or PRs for short)
+
+When submitting a pull request, make sure that your code is well commented and that your code is actually working. I will not be merging any PRs that is poorly written, missing comments and with poor programming style. In addition, your commit should include:
+
+- filename you've modified
+- what it does
+- why it is necessary
+
+For instance:
+
+run.rb: add support for x to allow y z
+
+# # Further Reading
+
+If you would like to learn more, simply refer to the code itself. It shouldn't be too hard to understand, since most of the code is documented using comments. If you have any questions, feel free to ask.
---
+++ run.rb
@@ -0,0 +1,268 @@
+=begin
+Written by Irvin.
+Distributed under the terms of the GNU General Public License v2.
+=end
+
+require 'rubygems'
+require 'zip'
+require 'yaml'
+require 'json'
+require 'fileutils'
+
+#
+# This class generates mods.yml file, which is derived from mcmod.info/litemod.json. 
+#
+class Config
+    
+# Constructor
+        def initialize( modsPath = "./mods/", configPath = "./config/", configFile = "mods.yml" )
+           @modsPath = modsPath
+           @configFile = configFile
+           @configPath = configPath
+           
+           if ( ! Dir.exists? @configPath )
+               Dir.mkdir( @configPath )
+           end
+           if ( ! File.exist? @configPath + @configFile )
+               file = File.new( @configPath + @configFile, "w+" )
+           end
+           if ( ! Dir.exist? @modsPath )
+               puts "Error! Path to mods folder does not exist!"
+               return nil
+           end
+        end
+        
+# Adds mods to config file
+        def append( filename = "", modslug = "", version = "" )
+            fileAppend = File.new( @configPath + @configFile, 'a' ) #r+
+            fileRead = File.new( @configPath + @configFile, 'r' ) 
+            
+            entry = {
+                modslug => {
+                        "version" => version,
+                        "filename" => filename
+                    }
+                }
+            
+# Append to file if file is empty. This is necessary to avoid generate initial yml file.
+            if ( File.zero? fileRead )
+                fileAppend.write ( entry.to_yaml )
+                fileRead.close()
+                fileAppend.close()
+                
+# If mods.yml is not empty, append by combining mods.yml file. This is necessary to prevent headers from being printed.
+            else
+                data = YAML.load( fileRead ) 
+                merged = data.merge( entry )
+                fileWrite = File.new( @configPath + @configFile, 'w' )
+                fileWrite.write( YAML.dump ( merged ) )
+                fileWrite.close()
+            end
+        end
+        
+# If mods.yml already has mod listed, update. If not, go to append method to populate that mod.
+        def update( filename = "", modslug = "", version = "" )
+            file = File.new( @configPath + @configFile, 'r+' )
+            data = YAML.load( file )
+            
+            if ( filename == "." || filename == ".." )
+                return nil
+            end
+
+            if ( ! File.zero? file )
+                if ( data.key?( modslug ) )
+                    data[modslug]["filename"] = filename
+                    data[modslug]["version"] = version
+                    fileWrite = File.new( @configPath + @configFile, 'w' )
+                    fileWrite.write ( data.to_yaml )
+                    fileWrite.close()
+                    file.close()
+                    return nil
+                end
+            end
+=begin            
+            if ( ! File.zero? file )
+                data.each do | keyone, valueone |
+                    if keyone == modslug
+                        valueone.each do | key, value |
+                                    exists = true
+                                    if ( key == "version" )
+                                        data[key] = version
+                                        puts "#{key} : #{value}"
+                                    elsif ( key == "filename" )
+                                        data[key] = filename
+                                        puts "#{key} : #{value}"
+                                    end
+                                end
+                    end
+                end
+            end
+=end
+            file.close()
+            append( filename, modslug, version )
+        end
+        
+# Get information from mcmod.info and litemod.json and pass that information to update method, which will pass to append method if necessary.
+        def populate()
+            puts "Working on directory: #{@modsPath}"
+            
+# Initalize variables
+            modslug = ""
+            version = ""
+            filename = ""
+            found = false
+            
+# Loop through all files in the mods directory
+            Dir.foreach( @modsPath ) do | current |
+                
+                absolute = @modsPath.to_s + current.to_s
+                puts "Currently populating: #{current}"
+                
+# If the file ends with .jar, for forge mods.
+                if ( File.extname( current ) == ".jar" )
+                    Zip::File.open ( absolute ) do | zip_file |
+                        if ( zip_file.find_entry( "mcmod.info" ) != nil )
+                            info = zip_file.read( "mcmod.info" )
+                            begin
+                                json = JSON.parse( info )[ 0 ]
+                                modslug = json['modid']
+                                version = json['version']
+                                found = true
+                                zip_file.close()
+                            rescue # Some mod devs have broken mcmod.info or weird parsing. This will prevent exceptions.
+                                zip_file.close()
+                                found = false
+                            end
+                        else # Some mods don't even contain mcmod.info!
+                            found = false
+                            zip_file.close()
+                        end
+                    end
+
+# If the file ends with .litemod, for litemods.
+                elsif ( File.extname( current ) == ".litemod" )
+                    Zip::File.open ( absolute ) do | zip_file |
+                        if ( zip_file.find_entry ( "litemod.json" ) )
+                            found = true
+                            info = zip_file.read( "litemod.json" )
+                            
+                            json = JSON.parse( info )
+                            modslug = json['name']
+                            version = json['mcversion'] + "-" + json['version']
+                        end
+                    end
+                end
+                
+                filename = current
+
+# If the loop above was not able to obtain information automatically, default back to filename.
+                if ( found == false )
+                    modslug = current
+                    version = "N/A"
+                end
+
+                update( filename, modslug, version )
+            end
+        end
+end
+
+#
+# This class uses generated mods.yml and creates .zip files for solder installation.
+#
+
+class Solder
+
+# Constructor
+    def initialize ( modsPath = "./mods/", configPath = "./config/", configFile = "mods.yml", outputPath = "./output/" )
+        @modsPath = modsPath
+        @configPath = configPath
+        @configFile = configFile
+        @outputPath = outputPath
+    end
+
+# Create folders in the output directory for archiving
+    def populate_folders
+        configFile = File.new( @configPath + @configFile, 'r' )
+        configHash = YAML.load( configFile )
+        
+        if ( ! File.zero? configFile )
+            configHash.each do | key, value |
+                directory = @outputPath + key + "/" + "mods"
+                puts directory
+                FileUtils.mkdir_p( directory )
+            end
+        end
+    end
+    
+# Create a copy of mods in the mods folder, to the directory we've just created.
+    def populate_mods
+        configFile = File.new( @configPath + @configFile, 'r' )
+        configHash = YAML.load( configFile )
+        
+        if ( ! File.zero? configFile )
+            configHash.each do | key, value |
+                file = @modsPath + configHash[key]["filename"]
+                destination = @outputPath + key + "/" + "mods" + "/"
+
+                if ( ! File.exist? destination + configHash[key]["filename"] )
+                    FileUtils.cp( file, destination )
+                end
+            end
+        end
+    end
+    
+# Create an archive (.zip) that includes mods folder and mod itself.
+    def populate_zips
+        configFile = File.new( @configPath + @configFile, 'r' )
+        configHash = YAML.load( configFile )
+        
+        if ( ! File.zero? configFile )
+            configHash.each do | key, value |
+                zipFile = @outputPath + key + "/" + key + "-" + configHash[key]["version"] + ".zip"
+                path = @outputPath + key + "/" + "mods" + "/"
+                File.delete( zipFile ) if File.exists?( zipFile )
+                Zip::File.open( zipFile, Zip::File::CREATE ) do | zip_file |
+                    zip_file.mkdir( "mods" )
+                    zip_file.add( "mods/" + configHash[key]["filename"], path + configHash[key]["filename"] )
+                    zip_file.close()
+                end
+            end
+        end
+    end
+
+# For now, this only gets rid of mods folder and mod itself.
+    def cleanup
+        configFile = File.new( @configPath + @configFile, 'r' )
+        configHash = YAML.load( configFile )
+        
+        if ( ! File.zero? configFile )
+            configHash.each do | key, value |
+                path = @outputPath + key + "/" + "mods" + "/"
+                FileUtils.rm_rf( path )
+            end
+        end
+
+    end
+end
+
+#
+# Essentially main method.
+#
+
+# Create new object for config
+config = Config.new()
+config.populate()
+
+# Stop and prompt the user to edit the config
+puts "Please edit the config file to ensure that all mods have correct: "
+puts "  * name: "
+puts "  * version: "
+puts "Hit ENTER when you are done."
+answer = gets.chomp!
+
+# Create new object for Solder
+action = Solder.new()
+action.populate_folders()
+action.populate_mods()
+action.populate_zips()
+action.cleanup()
